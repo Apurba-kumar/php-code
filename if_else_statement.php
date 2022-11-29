6b6aa81b710d73176eb0a01b66fcb6c72317def4
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-29 22:10:37 +0600

    if else statement

--- if_else_statement.php
+++ if_else_statement.php
@@ -1,9 +1,18 @@
 <?php
 $a = 26;
 if($a <= 25){
-    echo "you are eligible";
+    echo "you are eligible<br>";
 }
 else{
-    echo "you are not eligible";
+    echo "you are not eligible<br>";
 }
+$name="Apurba";
+$gender="male";
+
+if($gender=="male"){
+    echo "Hello Mr.".$name;
+}else{
+    echo "hello Mrs.".$name;
+}
+
 ?>
