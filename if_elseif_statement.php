804bfc5088c3419a3e4eacecc9710b3c3a0a53cd
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-29 22:18:26 +0600

    if_elseif_statement

---
+++ if_elseif_statement.php
@@ -0,0 +1,14 @@
+<?php
+$a = 18;
+if($a >=25){
+    echo "you are eligible";
+}
+elseif($a>=20 && $a<=22){
+    echo "You are perfect candidate.";
+}
+else
+{
+    echo "You are not eligibale";
+}
+
+?>
