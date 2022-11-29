e98744a91f9e009ff86fb7dd3d23a3569abf7bc2
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-29 12:38:07 +0600

    php basic

---
+++ print_echo.php
@@ -0,0 +1,11 @@
+<?php
+ echo "hello"," world<br>";
+ echo 'hello'.' world<br>';//echo return nothing
+//  print "hello","world"; because print can not display two string with coma.
+
+ print "hello". " world";//print return 1 .
+
+ /*main difference between print and echo : in echo we can print multiple
+  value but in print we can only print single value . */
+?>
+
---
+++ variables.php
@@ -0,0 +1,18 @@
+<?php
+//php a case sensitive language
+$a = "hello";
+$_a = 2;
+$a_b = 3;
+
+$aB = 5;
+$ab234=6;
+echo $a,$_a,$a_b,$aB,$ab234;
+
+//wrong way to declear a variables
+/* $first-name = "hello";
+$first name 
+$99firstname
+$first%name
+*/
+
+?>
