382b51be3ccb993c2f574040ec1478e11832e1d4
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-29 13:20:19 +0600

    constant variable

---
+++ constant.php
@@ -0,0 +1,12 @@
+<?php
+// case-insensitive constant name does not supported current version
+// define("GREETING", "Welcome to W3Schools.com!", true);
+// echo greeting;
+
+define("hello",20);
+// define("hello",30); we can not used same name varibale in constant
+echo hello;//can not use $ sign for constant variable name
+$hello = 30;
+echo $hello;
+?> 
+
