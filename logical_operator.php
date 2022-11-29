f18a983c86b777d2fd600486b86ddf9b11f63d18
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-29 22:02:26 +0600

    logical operator

---
+++ logical_operator.php
@@ -0,0 +1,15 @@
+<?php
+/* There are six logical operator are available in php
+&& logical AND
+|| logical OR
+!  logical NOT
+and logical and
+or  logical or
+xor exclusive OR
+*/
+$age = 18;
+if($age>=18 && $age<=25){
+    echo " You are eligible.";
+}
+
+?>
