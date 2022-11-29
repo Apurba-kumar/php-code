d010f9e8ac00ceea963a5279b0e36d8fcaf56643
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-29 21:56:08 +0600

    if statement

---
+++ if_statement.php
@@ -0,0 +1,14 @@
+<?php
+ $a = 10;
+ $b = 20;
+ if($a<$b){
+    echo "This is smaller!<br>";
+ }
+
+
+ //another way of declear if statement
+ if($a<$b):
+    echo "A is smaller.<br>";
+ endif;
+ echo " This is outside of the is statement."
+?>
