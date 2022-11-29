799420288217b5150ce281595a84013e507d7d5a
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-29 21:50:31 +0600

    comparison operator

---
+++ comparison_operator.php
@@ -0,0 +1,19 @@
+<?php
+$a = 20;
+$b = "20";
+
+echo $a+$b . "<br>";
+// == equal to
+// === equal value and equal type
+// != not equal
+// <> not equal
+// !== Not equal value or not equal type
+// > greater than
+// < less than
+// >= greater than or equal to
+// <= less than or equal to
+// <=> spaceship -> it returns -1, 0, 1 respectively less than , equal to, or greater than
+$x=10;
+$y=20;
+echo $x<=>$y;
+?>
