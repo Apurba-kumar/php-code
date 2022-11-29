c9bddc09549ebbc894c2f1a2597dfffc4f5182d1
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-29 22:56:53 +0600

    string operator

---
+++ string_operator.php
@@ -0,0 +1,12 @@
+<?php
+//we use for string operator concatenation . 
+$a= "hello ";
+$b = $a . 200;
+$c = $b . " world";
+echo $c."<br>";
+
+$x = "hello ";
+$x .= "world ";
+$x .= "people";
+echo $x;
+?>
