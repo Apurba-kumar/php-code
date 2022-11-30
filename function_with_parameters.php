ba272b5f8e0a4bccad6e65e7ff7ba34159e933c6
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-30 13:12:01 +0600

    function with parameter

--- function_with_parameters.php
+++ function_with_parameters.php
@@ -2,9 +2,10 @@
 function sum($num1,$num2){
     echo $num1 + $num2."<br>";
 }
-sum(10,20);
-function hello($fname, $lname){
+sum(10,20.55);
+function hello($fname="first", $lname="last"){
     echo "hello $fname $lname.<br>";
 }
 hello("apurba","kumar");
+hello("apurba");
 ?>
