dc52bc74378e9f4f65ec2a55cd9c12534a474772
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-11-30 18:20:28 +0600

    variable function or anonymous function

---
+++ variable_function.php
@@ -0,0 +1,13 @@
+<?php
+// function abc($x){
+// echo $x;
+// }        
+// $a = "abc";
+// $a("hello world");
+
+//another way to declear a variable function which called as a anonymous function
+$y = function($x){
+    echo $x;
+};
+$y("hello world! ");
+?>
