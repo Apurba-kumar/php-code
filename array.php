3fb862c63bf680c8e8e83d5e2831f48683b5f5bd
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-12-04 12:14:08 +0600

    array

---
+++ array.php
@@ -0,0 +1,21 @@
+<?php
+//  $arr = array(20,"red",50);//index array
+//  echo $arr[1]."<br>";
+//  //another way of declear an array
+//  $arry = [20,"hello",30];
+//  echo $arry[1];
+
+// $arr = array("hi","hello","byew");
+
+$arr[0] = "hi";
+$arr[1] = "hello";
+$arr[2] = "bye";
+
+
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
+
+// we can also print with for loop
+
+?>
