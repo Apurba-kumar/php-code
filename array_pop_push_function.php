21bc646a0b5b1fa286c5d83b5ebb17154dbc51cd
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-12-06 12:46:03 +0600

    array push and pop function

---
+++ array_pop_push_function.php
@@ -0,0 +1,15 @@
+<?php
+ $arr = ["aman","noman","raihan","tanyem"];
+
+ array_pop($arr);
+
+ echo "<pre>";
+ print_r($arr);
+ echo "</pre>";
+
+ array_push($arr,"Karim","kumar");
+ echo "<pre>";
+ print_r($arr);
+ echo "</pre>";
+
+?>
