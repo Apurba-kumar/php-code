4ff9e45d9b452426e01fbb113f2dc34788d42ab3
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-12-14 23:52:33 +0600

    string explode and implode function

---
+++ string_explode_implode.php
@@ -0,0 +1,23 @@
+<?php
+
+//this is for explode
+// $arr = "Hello world. It's a beautiful day!";
+
+// $array = explode(" ", $arr, 4/*here we can use -1,-2 for last value*/);//used for string value to array ,,,here we used space as a separator and third parameter for number of index we want
+
+// echo "<pre>";
+// print_r($array);
+// echo "</pre>";
+
+
+
+//this is for implode
+
+$arr = ["hello","world.","It's","a","beautiful","day!"];
+
+
+$array = implode(" ",$arr);
+
+echo "$array";
+
+?>
