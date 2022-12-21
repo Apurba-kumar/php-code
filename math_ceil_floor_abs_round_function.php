e527005e5c77a3bf11fde7051eb6ee32a3e68867
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-12-21 12:54:11 +0600

    Math ceil floor round abs functions

---
+++ math_ceil_floor_abs_round_function.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "The math ceil function:<br>";
+echo (ceil(0.60))."<br>";
+echo (ceil(0.50))."<br>";
+echo (ceil(0.40))."<br>";
+echo (ceil(5))."<br>";
+echo (ceil(5.1))."<br>";
+echo (ceil(-5.6))."<br>";
+
+echo "The math floor function:<br>";
+echo (floor(0.60))."<br>";
+echo (floor(0.40))."<br>";
+echo (floor(5))."<br>";
+echo (floor(5.1))."<br>";
+echo (floor(-5.6))."<br>";
+
+echo "The math round function:<br>";
+echo (round(0.60))."<br>";
+echo (round(0.40))."<br>";
+echo (round(5))."<br>";
+echo (round(5.1))."<br>";
+echo (round(-5.6))."<br>";
+echo "The math absolute function:<br>";
+echo (abs(0.60))."<br>";
+echo (abs(0.40))."<br>";
+echo (abs(5))."<br>";
+echo (abs(5.1))."<br>";
+echo (abs(-5.6))."<br>";
+?>
