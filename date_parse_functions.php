9a7af309348fe23a0e4341018b5f53f1dd144810
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-12-23 00:57:19 +0600

    date_parse funtions

---
+++ date_parse_functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// //date_parse(date)
+
+// echo "<pre>";
+// print_r(date_parse("2015-03-15 12:30:25:5"));
+// echo "</pre>";
+// $date = date_parse("2015-03-15 12:30:25:5");
+// echo  $date['day'];
+
+//date_parse_from_format(format, date)
+
+echo "<pre>";
+print_r(date_parse_from_format("d.n.Y", "15.3.2015"));
+echo "</pre>";
+
+?>
