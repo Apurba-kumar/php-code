0e3d1a0b307fe7a060543be1c5638910fdda3c1f
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-12-27 22:02:35 +0600

    $_GET $_POST $_REQUEST variables

--- $_GET_$_POST_$_REQUEST/form.php
+++ $_GET_$_POST_$_REQUEST/form.php
@@ -7,7 +7,8 @@
     <title>Form Page</title>
 </head>
 <body>
-    <form action="testform.php" method="get">
+    <!-- <form action="testform.php" method="get"> -->
+    <form action="testform.php" method="post">
         Name: <input type="text" name="fname"><br><br>
         Age : <input type="text" name="age"><br><br>
         <input type="submit" name="save">
---
+++ $_GET_$_POST_$_REQUEST/testform.php
@@ -0,0 +1,24 @@
+<?php
+
+// echo "<pre>";
+// print_r($_GET);
+// echo "</pre>";
+
+// echo $_GET['fname'];
+
+// //for post
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+// echo $_POST['fname'];
+
+
+//for request
+
+echo "<pre>";
+print_r($_REQUEST);
+echo "</pre>";
+
+echo $_REQUEST['fname'];
+?>
