cdc10776237cf4e65ba480c4329bf7916a61c9d2
Apurba-Kumar <apurba.kumar.cse@ulab.edu.bd>
2022-12-27 20:30:02 +0600

    string to time vise versa

--- strtotime_strftime_gmstrftime_functions.php
+++ strtotime_strftime_gmstrftime_functions.php
@@ -9,4 +9,13 @@ echo date("d-m-Y",strtotime("+1 week"))."<br>";
 echo date("d-m-Y H:i:s a",strtotime("+1 week 3 days 7 hours 45 minutes 5 seconds"))."<br>";
 echo date("d-m-Y",strtotime("next friday"))."<br>";
 echo date("d-m-Y",strtotime("last year"))."<br>";
+echo date("D",strtotime("last day of last month"))."<br>";
+
+
+echo strftime("%B %D %Y, %X %Z")."<br>";
+
+echo strftime("%B %D %Y, %X %Z", mktime(21,30,0,05,18,2005))."<br>";
+
+echo gmstrftime("%B %D %Y, %X %Z")."<br>";
+
 ?>
